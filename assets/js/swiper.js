6c4e8dfdeb4773b1c5f773b017f56723f1f9e34e
wind <shadowj7y@gmail.com>
2024-09-14 01:31:21 +0800

    first commit

---
+++ assets/js/swiper.js
@@ -0,0 +1,8 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
---
+++ main.js
@@ -0,0 +1,5 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import './assets/js/swiper';
+
+console.log("Hello world!");
